05e016ceebd12c301d04465f9e39399d557a557a
Ludmilla Aires Negri <ludaires@gmail.com>
2018-12-26 21:07:00 -0500

    Add MaintenancesController and maintenances views.

---
+++ app/controllers/maintenances_controller.rb
@@ -0,0 +1,25 @@
+class MaintenancesController < ApplicationController
+
+    def index
+        @maintenances = Maintenance.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end 
+
+    def update
+    end
+
+    def destroy
+    end  
+
+end
