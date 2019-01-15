1da161f2358133beb8b794ae7c4f9f54ca97e79f
Ludmilla Aires Negri <ludaires@gmail.com>
2019-01-14 23:01:58 -0500

    Include inverse of in has_many relationship.

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -1,7 +1,7 @@
 class Maintenance < ApplicationRecord
   belongs_to :user
   belongs_to :equipment
-  has_many :inspections
+  has_many :inspections, inverse_of: :maintenance
  
   accepts_nested_attributes_for :inspections
   validates :next_maintenance_date, :status, :maintenance_type, presence: :true
