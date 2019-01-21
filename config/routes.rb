461d3088c7a3165e23e39abb7cf84baf69f010de
Ludmilla Aires Negri <ludaires@gmail.com>
2019-01-21 11:28:55 -0500

    Create signup, login, logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,13 @@ Rails.application.routes.draw do
     resources :inspections
   end
 
-  # resources :inspections
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   delete '/inspections/:id/delete' => 'inspections#destroy'
 
