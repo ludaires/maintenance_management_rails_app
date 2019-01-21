1a7d1564f32dd9bb70dfe821373a768395ff846f
Ludmilla Aires Negri <ludaires@gmail.com>
2019-01-21 11:26:59 -0500

    Set up the sessions. Create login and logout actions.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+       @user = User.find_by(username: params[:username])
+       if @user  && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to maintenances_path
+       else
+        flash[:message] = "Something went wrong!"
+        redirect_to root_path
+       end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+    
+
+
+end
