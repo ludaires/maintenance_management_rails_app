c7ab053bc097c98447567ecce7874fee36c14475
Ludmilla Aires Negri <ludaires@gmail.com>
2019-01-21 11:26:06 -0500

    Create current_user and logged_in? methods and set them as helpers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+
+    private
+        def current_user
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+
+        def logged_in?
+            !!current_user
+        end
+
 end
