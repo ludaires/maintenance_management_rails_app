b5b70fe8229dc51d867fd368d12b6c51ca101b7e
Ludmilla Aires Negri <ludaires@gmail.com>
2019-01-25 23:25:54 -0500

    Add before_action set_maintenance.

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,41 +1,42 @@
 class InspectionsController < ApplicationController
-
+    before_action :set_maintenance, only: [:new, :create, :show, :edit, :update, :destroy]
 
     def new
-        @maintenance = Maintenance.find_by(params[:maintenance_id])
         @inspection = @maintenance.inspections.build
     end
 
     def create
-        @maintenance = Maintenance.find_by(params[:maintenance_id])
         @inspection = @maintenance.inspections.create(inspections_params)
        
         redirect_to maintenance_inspection_path(@inspection.maintenance, @inspection)
     end
 
     def show
-        @maintenance = Maintenance.find_by(id: params[:maintenance_id])
         @inspection = Inspection.find_by(id: params[:id])
     end
     
     def edit
-        @maintenance = Maintenance.find_by(id: params[:maintenance_id])
         @inspection = @maintenance.inspections.find_by(id: params[:id])
     end
 
     def update
-        @maintenance = Maintenance.find_by(id: params[:maintenance_id])
         @inspection = @maintenance.inspections.find_by(id: params[:id])
         @inspection.update(inspections_params)
         redirect_to maintenance_path(@maintenance)
     end
 
+    def destroy
+        @inspection = Inspection.find_by(id: params[:id])
+        @inspection.delete
+        redirect_to maintenance_path(@maintenance)
+    end
+
     private
+    def inspections_params
+        params.require(:inspection).permit(:part_id, :issue_id, :cause_id, :action_id)
+    end
 
-        def inspections_params
-            params.require(:inspection).permit(:part_id, :issue_id, :cause_id, :action_id)
-        end
-
-
-
+    def set_maintenance
+        @maintenance = Maintenance.find_by(id: params[:maintenance_id])
+    end
 end
