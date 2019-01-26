58aa42d01c9f60bf9b0e523fbd61ed6f91ecfe33
Ludmilla Aires Negri <ludaires@gmail.com>
2019-01-25 23:26:48 -0500

    Add method to redirect if user is not logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?, :current_user
-
+    helper_method :logged_in?, :current_user, :redirect_if_not_logged_in!
 
     private
-        def current_user
-            @current_user ||= User.find_by(id: session[:user_id])
-        end
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
 
-        def logged_in?
-            !!current_user
+    def logged_in?
+        !!current_user
+    end
+
+    def redirect_if_not_logged_in!
+        if !logged_in?
+            flash[:message] = "You must be logged to complete this action"
+            redirect_back fallback_location: root_path
         end
+    end
 
 end
