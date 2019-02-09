d6771472ccd1a678f23dd4c3684b53b29b790c3b
Ludmilla Aires Negri <ludaires@gmail.com>
2019-02-08 21:36:49 -0500

    Add scope method to order equipment using the next maintenance date.

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -6,6 +6,8 @@ class Maintenance < ApplicationRecord
   accepts_nested_attributes_for :inspections
   validates :next_maintenance_date, :status, :maintenance_type, presence: :true
 
+  scope :ordered_by_next_maintenance, -> { order(next_maintenance_date: :desc).limit(5)}
+
   #show first all maintenance in progress
 
 end
