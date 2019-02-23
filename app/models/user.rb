73f151e01fefaea76f8481e9a1144156b9d22961
Ludmilla Aires Negri <ludaires@gmail.com>
2019-02-23 10:33:55 -0500

    Add a method to create a user using information from google.(auth)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
 
     validates :username, :email, presence: :true, uniqueness: :true
     validates :username, uniqueness: {case_sensitive: :false}
+    # email format is correct
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+    def self.create_from_google(auth)
+        User.find_or_create_by(email: auth[:info][:email]) do |user|
+            user.username = auth[:info][:name]
+            user.password = SecureRandom.hex
+        end
+    end
+
+
 end
